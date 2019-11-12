19e9b91d532f20d754e990e7712dfd264fdded9a
Jackson Newlands <32347226+jackson097@users.noreply.github.com>
2019-11-12 15:59:42 -0500

    Optimized code for better organization and less messy code.

--- Exam_Calculator.py
+++ Exam_Calculator.py
@@ -1,55 +1,49 @@
-# Inputs
-current_mark = float(input("Enter current mark (%):"))
-while current_mark < 0:
-    print("Error. Please enter a positive number.")
-    current_mark = float(input("Enter current mark (%):"))
+from functions import is_valid, calculate_mark_needed
 
-desired_mark = float(input("Enter desired mark (%):"))
-while desired_mark < 0:
-    print("Error. Please enter a positive number.")
-    desired_mark = float(input("Enter desired mark (%):"))
+# User Inputs
+current_mark = raw_input("Enter current mark (%): ")
+while is_valid(current_mark) == False:
+    current_mark = raw_input("Number not valid.\nEnter current mark (%):")
 
-exam_weight = float(input("Enter exam weight (%):"))
-while exam_weight < 0:
-    print("Error. Please enter a positive number.")
-    exam_weight = float(input("Enter exam weight (%):"))
+desired_mark = raw_input("Enter desired mark (%):")
+while is_valid(desired_mark) == False:
+    desired_mark = raw_input("Number not valid.\nEnter desired mark (%):")
 
-# Valid number check
-if current_mark < 0 or current_mark > 100:
-    print("Error. Not a valid value.")
-elif exam_weight < 0 or exam_weight > 100:
-    print("Error. Not a valid value.")
-elif desired_mark < 0 or desired_mark > 100:
-    print("Error. Not a valid value.")
+exam_weight = raw_input("Enter exam weight (%):")
+while is_valid(exam_weight) == False or exam_weight == '0':
+    exam_weight = raw_input("Number not valid.\nEnter exam weight (%):")
 
-# Calculations
-exam_weight = exam_weight / 100
-course_work_weight = 1 - exam_weight
-current_mark = (current_mark * course_work_weight)
-mark_needed = desired_mark - current_mark
-mark_needed = mark_needed / exam_weight
+	
+# Calculate the mark needed
+mark_needed = calculate_mark_needed(float(current_mark), float(desired_mark), float(exam_weight)/100)
 
-# Print Statements
+
+# Print results
+print_statement = "You will need a {:.1f}% on the final exam for a final mark of {:.1f}%. ".format(float(mark_needed), float(desired_mark))
+
 if mark_needed > 100:
-    print("You will need a {:.1f}% on the final exam for a final mark of {:.1f}%. You're screwed.".format(
-        mark_needed, desired_mark))
-elif mark_needed >= 90 and mark_needed < 100:
-    print("You will need a {:.1f}% on the final exam for a final mark of {:.1f}%. Anything's possible.".format(
-        mark_needed, desired_mark))
+    print(print_statement + "You're screwed.")
+	
+elif mark_needed >= 90 and mark_needed <= 100:
+    print(print_statement + "Anything's possible.")
+	
 elif mark_needed >= 80 and mark_needed < 90:
-    print("You will need a {:.1f}% on the final exam for a final mark of {:.1f}%. You can do it!".format(
-        mark_needed, desired_mark))
+    print(print_statement + "You can do it!")
+	
 elif mark_needed >= 70 and mark_needed < 80:
-    print("You will need a {:.1f}% on the final exam for a final mark of {:.1f}%. Not too bad.".format(
-        mark_needed, desired_mark))
+    print(print_statement + "Not too bad.")
+	
 elif mark_needed >= 60 and mark_needed < 70:
-    print("You will need a {:.1f}% on the final exam for a final mark of {:.1f}%. Shouldn't be too hard.".format(
-        mark_needed, desired_mark))
+    print(print_statement + "Shouldn't be too hard.")
+	
 elif mark_needed >= 50 and mark_needed < 60:
-    print("You will need a {:.1f}% on the final exam for a final mark of {:.1f}%. Should be a breeze.".format(
-        mark_needed, desired_mark))
-elif mark_needed < 50:
-    print("You will need a {:.1f}% on the final exam for a final mark of {:.1f}%. Just show up and you're good.".format(
-        mark_needed, desired_mark))
+    print(print_statement + "Should be a breeze.")
+	
+elif mark_needed < 50 and mark_needed > 0:
+    print(print_statement + "Just show up and you're good.")
+	
+elif mark_needed <= 0:
+	print(print_statement + "Don't even bother...")
+	
 else:
     print("Error.")
